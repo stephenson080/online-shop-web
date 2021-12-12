6c571831ab6a841616807f9a0b2fc7d93a054950
stephenson080 <stevepathagoras08@gmail.com>
2021-12-12 01:54:26 +0100

    correct mongodb

--- util/database.js
+++ util/database.js
@@ -4,9 +4,8 @@ let _db
 
 const mongoConnection = (callBackFn)=>{
     MongoClient
-    .connect("mongodb+srv://stephen:pathagoras1555@cluster0.xrlnc.mongodb.net/online-shop?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true})
+    .connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(client=>{
-        console.log("Connected!")
         _db = client.db()
         callBackFn()
     })
@@ -26,27 +25,3 @@ exports.mongoConnection = mongoConnection
 exports.getDatabase = getDatabase
 
 
-// const {Sequelize} = require("sequelize")
-
-// const sequelize = new Sequelize("online-shop", "postgres", "pathagoras1555", {
-//     dialect: "postgres",
-//     host: "localhost",
-//     port: "5433"
-// })
-// exports.pool = new Pool({
-//     host: "localhost",
-//     user: "postgres",
-//     password: "pathagoras1555",
-//     port: "5433",
-//     database: "online-shop"
-// })
-// const client = new Client({
-//     host: "localhost",
-//     user: "postgres",
-//     password: "pathagoras1555",
-//     port: "5433",
-//     database: "online-shop"
-// })
-
-// client.connect()
-// module.exports = sequelize
